ecd4dd65f96e03ada138faaa48c54d3517994432
David Scott <dvdscott23@gmail.com>
2025-06-21 16:59:46 +0100

    update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
-
-from quintin_bot import bot  # This makes sure the bot starts
 from http.server import BaseHTTPRequestHandler
-from io import BytesIO
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(b"Quintin is alive and brewing stew.")
+        self.wfile.write(b"Quintin is alive. The stew smells good.")
